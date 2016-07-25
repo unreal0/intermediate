f0878c06fd024ed4f8410d422a7ef19a5973427f
unreal0 <unreal0@sina.cn>
2016-07-25 21:53:20 +0800

    new

---
+++ client/recipes/recipes.js
@@ -0,0 +1,20 @@
+// Meteor.subscribe('recipes')
+Template.Recipes.onCreated(function(){
+	var self = this
+	self.autorun(function(){
+		self.subscribe('recipes')
+	})
+})
+
+Template.Recipes.helpers({
+	recipes: () => {
+		return Recipes.find()
+	}
+})
+
+Template.Recipes.events({
+	'click .new-recipe':() =>{
+		Session.set('newRecipe', true);
+	}
+})
+// console.log(Meteor.settings.public.ga.account);
